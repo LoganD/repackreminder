3a2cc53c46f9735af116744bbb6f16d96845871d
Logan Donovan <commit@logandonovan.com>
2017-01-07 21:33:47 -0500

    remove extra migration

--- priv/repo/migrations/20170107230431_update_reminder_table.exs
+++
@@ -1,9 +0,0 @@
-defmodule RepackReminder.Repo.Migrations.UpdateReminderTable do
-  use Ecto.Migration
-
-  def change do
-    alter table(:reminders) do
-      add :reminder_offset, :integer
-    end
-  end
-end
