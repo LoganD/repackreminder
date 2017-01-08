492a83252af7470619acb658d2b1048186cd4257
Logan Donovan <commit@logandonovan.com>
2017-01-07 21:11:50 -0500

    updated config for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -26,7 +26,7 @@ config :logger, level: :info
 #
 #     config :repack_reminder, RepackReminder.Endpoint,
 #       ...
-#       url: [host: "example.com", port: 443],
+#       url: [scheme: "https", host: "mysterious-meadow-6277.herokuapp.com", port: 443],
 #       https: [port: 443,
 #               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
 #               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
@@ -60,10 +60,10 @@ config :logger, level: :info
 # which should be versioned separately.
 config :hello_phoenix, HelloPhoenix.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "mysterious-meadow-6277.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "quiet-beach-67024.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: System.get_env("q7VOyoKVnRoVcQ5H27FRReIC8pBsOHYl1yeLnHdqQPGF6FR6+aMfPyh8/AZUqLam")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -71,6 +71,6 @@ config :logger, level: :info
 # Configure your database
 config :hello_phoenix, HelloPhoenix.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
+  url: System.get_env("postgresql-parallel-59531"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
